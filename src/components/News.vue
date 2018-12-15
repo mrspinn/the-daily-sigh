2ea3af56e2ae3dc77e3602cf7b0fb176b12206a7
Spencer Frost <s.s.frost@gmail.com>
2018-12-15 10:16:35 -0500

    Add favicon

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,62 +1,67 @@
 <template>
     <div class="news">
-        <div v-for="story in topStories" :key="story.url">
-            <v-layout row class="justify-center">
-                <v-flex xs12 sm10 md8>
-                    <v-card>
-                        <v-card-title class="headline">
-                            {{story.title}}
-                        </v-card-title>
-                        <v-card-text>
-                            {{story.abstract}}
-                        </v-card-text>
-                    </v-card>
-                    <v-divider></v-divider>
-                </v-flex>
+        <a href="#app" v-smooth-scroll="{duration:500}" class="toTop"><v-icon app click="">keyboard_arrow_up</v-icon></a>
+        <v-container class="pt-0" style="position: relative;">
+              
+            <v-layout class="justify-end mb-2">
+                <v-menu offset-y bottom right flat>
+                    <v-btn small slot="activator">Stories per page</v-btn>
+                    <template v-for="number in pageArray">
+                        <v-chip
+                            small
+                            :key="number"
+                            @click="setStoriesPerPage(number)">
+                            {{number}}
+                        </v-chip>
+                    </template>
+                </v-menu>
             </v-layout>
-            
-        </div>
+
+            <app-article v-for="story in $store.getters.getStories.slice(0, this.storiesPerPage)" :key="story.url" :story="story"></app-article>
+        </v-container>
     </div>
 </template>
 
 <script>
-import axios from "axios";
+import Article from "./Article.vue";
+import { mapGetters } from 'vuex';
+
 export default {
     data() {
         return {
-            topStories: null
+            pageArray: [5, 10, 15, 25, 50],
+            storiesPerPage: 5
         }
     },
+    methods: {
+        setStoriesPerPage(num) {
+            this.storiesPerPage = num;
+        }
+    },
+<<<<<<< HEAD
     mounted () {
         
         axios
         .get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
         .then(response => {this.topStories = response.data.results; console.log(response)});
+||||||| parent of af35134... Add stories per page feature. Add smooth scroll to top feature.
+    mounted () {
+        
+        axios
+        .get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4482d9dc32bd470aa61fb2f15b3ad968")
+        .then(response => {this.topStories = response.data.results; console.log(response)});
+=======
+    components: {
+        appArticle: Article
+>>>>>>> af35134... Add stories per page feature. Add smooth scroll to top feature.
     }
 }
 </script>
 
 <style>
-    /* .news {
-        width: 80%;
-        margin: 0 auto;
-        border-right: 1px solid #aaa;
-        height: 100%;
+    .toTop {
+        position: fixed;
+        top: 1rem;
+        right: 1rem
     }
-    .imageDiv {
-        float: left;
-        width: 40%;
-        margin-right: 1rem;
-        margin-bottom: 1rem;
-    }
-    .image {
-        background: red; 
-        height: 300px;
-    }
-    .caption {
-        font-size: .6rem;
-    }
-    .content {
-        display: inline;
-    } */
 </style>
