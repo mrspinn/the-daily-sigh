4316fb329cbe634196b81c98f4529341ef126148
mrspinn <34923439+mrspinn@users.noreply.github.com>
2018-12-16 18:31:48 -0500

    Delete axios-config.js from root

--- axios-config.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'https://api.nytimes.com/svc/topstories/v2',
-  params: { 'api-key': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' }
-
-})
-
-export default instance
